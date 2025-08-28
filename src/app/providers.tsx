baf7c759a1a1749bdedf77d118badc35a13e3f6a
teddzyb <teddybartletg88@gmail.com>
2025-08-28 15:50:40 +0800

    feat: update dependencies and add HeroUI integration
    - Added @heroui/react, framer-motion, next-themes to dependencies.
    - Updated globals.css for new theme variables and fonts.
    - Created hero.ts to initialize HeroUI.
    - Added Providers component for HeroUI and NextThemes integration.

---
+++ src/app/providers.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { HeroUIProvider, ToastProvider } from "@heroui/react"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>["push"]>[1]>;
+  }
+}
+
+export function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+  // note: read heroui docs if you are using custom basePath
+
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        <ToastProvider
+          toastProps={{
+            classNames: {
+              base: "lg:bottom-[10px]! lg:right-[10px]!",
+            },
+          }}
+
+        />
+        {children}
+      </NextThemesProvider>
+    </HeroUIProvider>
+  )
+}
