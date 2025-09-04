3773c9fc09c1102e0d418f9e01a93628c2a3f074
teddzyb <teddybartletg88@gmail.com>
2025-09-05 01:25:30 +0800

    feat: restructure MainLayout to include navigation for improved layout

--- src/app/_components/layouts/MainLayout.tsx
+++ src/app/_components/layouts/MainLayout.tsx
@@ -1,8 +1,18 @@
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-h-screen flex flex-col">
-      {children}
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <nav className="h-16 border-b border-default-200 flex items-center justify-center px-4">
+        <div className="container flex items-center justify-between">
+          <div className="text-3xl font-semibold px-2 text-primary font-logo">
+            Random
+          </div>
+        </div>
+      </nav>
+
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+    </div>
   );
 };
 
